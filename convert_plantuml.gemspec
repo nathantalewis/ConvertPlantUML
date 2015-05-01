601b2f75f3dc96831c712248d32e42ebc5f98009
Nathan T.A. Lewis <nathan@nathantalewis.com>
2015-05-01 10:06:59 -0700

    Rubocop improvements.

--- convert_plantuml.gemspec
+++ convert_plantuml.gemspec
@@ -7,9 +7,14 @@ Gem::Specification.new do |s|
   s.author	= 'Nathan T.A. Lewis'
   s.email	= 'nathan@nathantalewis.com'
   s.summary	= 'Command line tool to convert PlantUML files'
-  s.description	= 'convert_plantuml allows you to convert your PlantUML files to SVG, PNG, or Ascii using a remote server of your choice'
-  s.files	= ['lib/convert_plantuml.rb', 'vendor/plantuml-encode64.rb', 'bin/convert_plantuml']
-  s.require_paths = ['lib', 'vendor']
+  s.description	= 'convert_plantuml allows you to convert your PlantUML files '\
+                  'to SVG, PNG, or Ascii using a remote server of your choice'
+  s.files	= %w(
+    lib/convert_plantuml.rb
+    vendor/plantuml-encode64.rb
+    bin/convert_plantuml
+  )
+  s.require_paths = %w('lib', 'vendor')
   s.executables	<< 'convert_plantuml'
   s.license	= 'Apache 2.0'
 end
--- lib/convert_plantuml.rb
+++ lib/convert_plantuml.rb
@@ -1,24 +1,31 @@
 require 'open-uri'
 require 'plantuml-encode64'
 
+# Converts PlantUML files to a rendered format using a remote server.
+#
 class ConvertPlantUML
+  class << self
+    # TODO: Replace verbose flag with proper logging
+    # TODO: Add 'ensures' to make sure the files are closed
+    def convert(input: nil, output: nil, type: 'png',
+                server: URI.parse('http://www.plantuml.com/plantuml/'),
+                verbose: false)
+      $stderr.puts "Retrieving data from #{encoded_uri}" if verbose
+      open(URI.join(server, "#{type}/",
+                    plantuml_encode_from_file_or_stdin(input))) do |response|
+        copy_to = ->(file) { IO.copy_stream(response, file) }
+        $stderr.puts "Writing output file... #{output}" if verbose
+        output ? open(output, 'w', &copy_to) : copy_to.call($stdout)
+      end
+    end
 
-  #TODO: Replace verbose flag with proper logging
-  #TODO: Add 'ensures' to make sure the files are closed
-  def self.convert(input: nil, output: nil, type: 'png', server: URI.parse("http://www.plantuml.com/plantuml/"), verbose: false)
-    $stderr.puts "Reading input file... #{input}" if verbose
-    inputFile = if input then open(input) else $stdin end
-    inputString = inputFile.read
-    inputFile.close
+    private
 
-    encodedURI = URI.join(server, "#{type}/", PlantUmlEncode64.new(inputString).encode())
-    $stderr.puts "Retrieving data from #{encodedURI}" if verbose
-
-    response = open(encodedURI) do |response|
-      $stderr.puts "Writing output file... #{output}" if verbose
-      outputFile = if output then open(output, 'w') else $stdout end
-      IO.copy_stream(response, outputFile)
-      outputFile.close
+    def plantuml_encode_from_file_or_stdin(input)
+      read_from = ->(file) { file.read }
+      PlantUmlEncode64.new(
+        input ? open(input, &read_from) : read_from.call($stdin)
+      ).encode
     end
   end
 end
